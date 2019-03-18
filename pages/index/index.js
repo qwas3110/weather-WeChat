df8e5db6c15602ae8fe61dd58ee6c8becaf1da70
qwas3110 <283994917@qq.com>
2019-03-18 13:11:36 +0800

    update temp and weather in setData

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,29 +1,33 @@
 Page({
 
-	data:{
-		string: 'Hello world',
-		temp: '12°',
-		weather: '晴天'
-	},
+  data: {
+    string: 'Hello world',
+    nowTemp: '12°',
+    nowWeather: '晴天'
+  },
 
-	onLoad() {
-		wx.request({
-			url: 'https://test-miniprogram.com/api/weather/now',
-			data: {
-				city: '广州'
-			},
-			header: {
-				'content-type': 'application/json' // 默认值
-			},
-			// 调取成功后使用的方法
-			success(res) {
-				console.log(res.data)
-				const result = res.data.result;
-				const temp = result.now.temp + "°";
-				const weather= result.now.weather;
-				console.log(temp,weather);
-				
-			}
-		})
-	}
+  onLoad() {
+    wx.request({
+      url: 'https://test-miniprogram.com/api/weather/now',
+      data: {
+        city: '广州'
+      },
+      header: {
+        'content-type': 'application/json' // 默认值
+      },
+      // 调取成功后使用的方法
+      success: (res) => {
+        console.log(res.data)
+        const result = res.data.result;
+        const temp = result.now.temp + "°";
+        const weather = result.now.weather;
+        console.log(temp, weather);
+				this.setData({
+					nowTemp:temp,
+					nowWeather:weather
+				})
+
+      }
+    })
+  }
 })
