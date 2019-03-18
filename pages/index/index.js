cf618628ec1fe0b66f2274ea2455d5d0297aca9a
qwas3110 <283994917@qq.com>
2019-03-18 11:57:33 +0800

    get form weather API

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,3 +1,29 @@
 Page({
-  
+
+	data:{
+		string: 'Hello world',
+		temp: '12°',
+		weather: '晴天'
+	},
+
+	onLoad() {
+		wx.request({
+			url: 'https://test-miniprogram.com/api/weather/now',
+			data: {
+				city: '广州'
+			},
+			header: {
+				'content-type': 'application/json' // 默认值
+			},
+			// 调取成功后使用的方法
+			success(res) {
+				console.log(res.data)
+				const result = res.data.result;
+				const temp = result.now.temp + "°";
+				const weather= result.now.weather;
+				console.log(temp,weather);
+				
+			}
+		})
+	}
 })
