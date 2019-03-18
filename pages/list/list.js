357ca16b83578c6ecfc75dbf4ec9457f4d29d581
qwas3110 <283994917@qq.com>
2019-03-19 00:22:17 +0800

    第二个页面获取数据

--- pages/list/list.js
+++ pages/list/list.js
@@ -1,9 +1,49 @@
+const dayMap = ['星期日', '星期一', '星期二', '星期三', '星期四', '星期五', '星期六']
 
 Page({
-	data: {
-		list: [1,2,3,4,5,6,7]
+  data: {
+    weekWeather: []
+  },
+	onPullDownRefresh() {
+		this.getWeekWeather(() => {wx.stopPullDownRefresh()});
 	},
 	onLoad() {
+		this.getWeekWeather();
+	},
+	getWeekWeather(callback) {
+		wx.request({
+			url: 'https://test-miniprogram.com/api/weather/future',
+			data: {
+				city: '广州市',
+				time: new Date().getTime()
+			},
+			success: (res) => {
+				console.log(res.data.result);
+				const result = res.data.result;
+				this.setWeekWeather(result);
+			},
+			complete: () => {
+				callback && callback();
+			}
+		})
+	},
+	setWeekWeather(result) {
+		let weekWeather = [];
+		let date = new Date();
+
+		for (let x = 0; x < result.length; x++) {
+			weekWeather.push({
+				day: `${dayMap[x]}`,
+				date: `${date.getFullYear()}-${date.getMonth() +x}-${date.getDate()}`,
+				imgSrc: `/img/${result[x].weather}-icon.png`,
+				temp: `${result[x].minTemp}°-${result[x].maxTemp}°`
+			})
+		};
+		weekWeather[0].day = '今天';
+		this.setData({weekWeather})
 
 	}
+
+
+
 })
