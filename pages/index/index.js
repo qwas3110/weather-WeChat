099ca2575d85457e95f1cd82c7a13fb826e6e502
qwas3110 <283994917@qq.com>
2019-03-19 11:27:30 +0800

    使用腾讯地址 逆向转换城市名

--- pages/index/index.js
+++ pages/index/index.js
@@ -17,6 +17,8 @@ const weatherColorMap = {
   'snow': '#aae1fc'
 }
 
+// 引入SDK核心类
+const QQMapWX = require('../../libs/qqmap-wx-jssdk.js')
 
 Page({
 
@@ -25,18 +27,23 @@ Page({
     nowTemp: '12°',
     nowWeather: '晴天',
     imgSrc: ``,
-		hourlyWeather: [],
-		todayTemp:'',
-		todayDate: ''
+    hourlyWeather: [],
+    todayTemp: '',
+    todayDate: ''
   },
   onPullDownRefresh() {
-		// 传入回调函数，当我自己刷新更新后，我就会停止
-		this.getNow(() => {wx.stopPullDownRefresh()});
+    // 传入回调函数，当我自己刷新更新后，我就会停止
+    this.getNow(() => {
+      wx.stopPullDownRefresh()
+    });
   },
 
-	onLoad() { 
-		this.getNow();
-	},
+  onLoad() {
+    this.qqmapsdk = new QQMapWX({
+			key: 'H2TBZ-7XC6P-765DS-VGG53-QDCGQ-WEFMX'
+    });
+    this.getNow();
+  },
 
   //将调用数据单独写一个模块，方便调用刷新 
   getNow(callback) {
@@ -51,72 +58,97 @@ Page({
       // 调取成功后使用的方法
       success: (res) => {
         console.log(res.data)
-				// 获取数据
-				let result = res.data.result
-				// 调用
+        // 获取数据
+        let result = res.data.result
+        // 调用
         this.setNow(result);
-				this.setHourlyWeather(result);
-				this.setToady(result);
+        this.setHourlyWeather(result);
+        this.setToady(result);
       },
-			// 当完成数据更新后，关闭下拉刷新，在complete完成函数内执行
-			complete: () => {
-				// 意思就是如果有回调函数传入，是true，我就执行下一条，就是去执行
-				// 没传入就不执行
-				callback && callback()
-			}
+      // 当完成数据更新后，关闭下拉刷新，在complete完成函数内执行
+      complete: () => {
+        // 意思就是如果有回调函数传入，是true，我就执行下一条，就是去执行
+        // 没传入就不执行
+        callback && callback()
+      }
     })
   },
-	// 显示当前天气
-	setNow(result) {
-		const temp = result.now.temp + "°";
-		const weather = result.now.weather;
-		const imgSrc = `/img/${weather}-bg.png`;
-		console.log(temp, weather, imgSrc);
+  // 显示当前天气
+  setNow(result) {
+    const temp = result.now.temp + "°";
+    const weather = result.now.weather;
+    const imgSrc = `/img/${weather}-bg.png`;
+    console.log(temp, weather, imgSrc);
     // 同步到视图 
-		this.setData({
-			nowTemp: temp,
-			nowWeather: weatherMap[weather],
-			imgSrc: imgSrc,
-		});
-		// 动态设置导航栏颜色
-		wx.setNavigationBarColor({
-			frontColor: '#ffffff',
-			backgroundColor: weatherColorMap[weather]
-		})
-	},
-	// 显示未来几个小时的天气
-	setHourlyWeather(result) {
-		// 设置时间 3个小时一次
-		// 创建一个空数组
-		let hourlyWeather = [];
-		// 引用数据
-		let forecast = result.forecast;
-		// 引入时间
-		let nowHour = new Date().getHours();
-		// 循环，将数据修改，导入相应位置
-		for (let x = 0; x < 8; x++) {
-			hourlyWeather.push({
-				time: `${(x * 3 + nowHour) % 24}时`,
-				icon: `/img/${forecast[x].weather}-icon.png`,
-				temp: `${forecast[x].temp}°`
-			})
-		};
-		hourlyWeather[0].time = "当前";
-		this.setData({ hourlyWeather: hourlyWeather })
-	},
-	// 当天最高最低气温
-	setToady(result) {
-		let date = new Date();
-		let today = result.today;
-		this.setData({
-			todayTemp: `${today.minTemp}° - ${today.maxTemp}°`,
-			todayDate: `${date.getFullYear()}-${date.getMonth()+1}-${date.getDate()} 今天`
-		})
-	},
-	// 点按事件
-	onTapDayWeather() {
-		wx.navigateTo({
-			url: "/pages/list/list"
-		})
-	}
+    this.setData({
+      nowTemp: temp,
+      nowWeather: weatherMap[weather],
+      imgSrc: imgSrc,
+    });
+    // 动态设置导航栏颜色
+    wx.setNavigationBarColor({
+      frontColor: '#ffffff',
+      backgroundColor: weatherColorMap[weather]
+    })
+  },
+  // 显示未来几个小时的天气
+  setHourlyWeather(result) {
+    // 设置时间 3个小时一次
+    // 创建一个空数组
+    let hourlyWeather = [];
+    // 引用数据
+    let forecast = result.forecast;
+    // 引入时间
+    let nowHour = new Date().getHours();
+    // 循环，将数据修改，导入相应位置
+    for (let x = 0; x < 8; x++) {
+      hourlyWeather.push({
+        time: `${(x * 3 + nowHour) % 24}时`,
+        icon: `/img/${forecast[x].weather}-icon.png`,
+        temp: `${forecast[x].temp}°`
+      })
+    };
+    hourlyWeather[0].time = "当前";
+    this.setData({
+      hourlyWeather: hourlyWeather
+    })
+  },
+  // 当天最高最低气温
+  setToady(result) {
+    let date = new Date();
+    let today = result.today;
+    this.setData({
+      todayTemp: `${today.minTemp}° - ${today.maxTemp}°`,
+      todayDate: `${date.getFullYear()}-${date.getMonth()+1}-${date.getDate()} 今天`
+    })
+  },
+  // 点按事件
+  onTapDayWeather() {
+    wx.navigateTo({
+      url: "/pages/list/list"
+    })
+  },
+
+  // 获取经纬度
+  onTapLocation() {
+
+    wx.getLocation({
+			type: 'gcj02',
+      success: (res) => {
+				console.log(res.longitude,res.latitude);
+				this.qqmapsdk.reverseGeocoder({
+					location: {
+						latitude: res.latitude,
+						longitude: res.longitude
+					},
+					success: (res) => {
+						console.log(res);
+						console.log(res.result.address_component.city);
+						const city = res.result.address_component.city;
+					}
+				})
+      }
+
+    })
+  }
 })
